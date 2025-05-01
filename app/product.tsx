9a4c9856c56559f5335c60161297095c14f4c0c0
Gary Ascuy <gary.ascuy@gmail.com>
2025-04-30 22:51:01 -0500

    Adding route implementation

--- app/product.tsx
+++ app/product.tsx
@@ -2,12 +2,23 @@
 import type { Route } from '.react-router/types/app/+types/product';
 
 export async function loader({ params }: Route.LoaderArgs) {
-    let product = { name: "Product Name", pid: params.pid };
+    let product = { name: "Mac Mini m4", pid: params.pid };
     return { product };
 }
 
 export default function Product({
     loaderData,
 }: Route.ComponentProps) {
-    return <div>{loaderData.product.name}</div>;
+    return (
+        <div className='py-8 px-30'>
+            <h1 className='text-2xl font-bold'>Product ({loaderData.product.pid})</h1>
+
+            <div className="grid grid-cols-2 gap-4 mt-8">
+                <div className="text-blue-400 font-bold">ID</div>
+                <div className="italic">{loaderData.product.pid}</div>
+                <div className="text-blue-400 font-bold">Name</div>
+                <div className="italic">{loaderData.product.name}</div>
+            </div>
+        </div>
+    );
 }
